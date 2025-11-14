b56d6c33983555122601bdc701d2bcec8dafebb7
Guilhem Saurel <guilhem.saurel@laas.fr>
2025-11-14 01:46:38 +0100

    ip

--- machines/base-pi/configuration.nix
+++ machines/base-pi/configuration.nix
@@ -1,4 +1,11 @@
 {
   networking.hostName = "base-pi";
   nixpkgs.hostPlatform = "aarch64-linux";
+
+  networking.interfaces.end0.ipv4.addresses = [
+    {
+      address = "192.168.100.100";
+      prefixLength = 24;
+    }
+  ];
 }
